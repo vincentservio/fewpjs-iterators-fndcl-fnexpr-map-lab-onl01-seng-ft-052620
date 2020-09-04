5418342c4df03bcc75594bd689c9e4d1db8ffe49
Vincent Servio <vincentservio@gmail.com>
2020-09-04 03:51:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,3 +16,19 @@ const titleCased = () => {
 }
 
 
+const robots = [
+  { name: 'Johnny 5', modes: 5, isActivated: false, },
+  { name: 'C3PO', modes: 3, isActivated: false, },
+  { name: 'Sonny', modes: 2.5, isActivated: false, },
+  { name: 'Baymax', modes: 1.5, isActivated: false, },
+];
+ 
+const activatedRobots = robots.map(function (robot) {
+  return Object.assign({}, robot, {
+    modes: robot.modes * 2,
+    isActivated: true,
+  });
+});
+ 
+console.log(activatedRobots);
+ 
